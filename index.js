fb92f703c7cf54ad4b472bb49cbb0658bb70b994
Abdi878 <k21071276@kcl.ac.uk>
2023-01-03 08:47:32 +0000

    Add colour change when hovered

--- index.js
+++ index.js
@@ -3,3 +3,11 @@ container = document.querySelector(".container")
 for(let i = 0; i<256;i++){
     container.appendChild(document.createElement("div"))
 }
+
+divs = document.querySelectorAll(".container>div")
+
+divs.forEach(function(div){
+    div.addEventListener("mouseover",function(e){
+        div.style.backgroundColor="black";
+    })
+})
