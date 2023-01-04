aa9ca72f4ed613637684a56c54b42d2cf6b9ac03
Abdi878 <k21071276@kcl.ac.uk>
2023-01-04 03:02:41 +0000

    Add option to change grid size

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
 body = document.querySelector("body")
 container = document.querySelector(".container")
-for(let i = 0; i<256;i++){
-    container.appendChild(document.createElement("div"))
+function gridCreation(sideLength=16){
+    container.innerHTML=""
+    let squareNum = sideLength**2;
+    for(let i = 0; i<squareNum;i++){
+        newDiv = document.createElement("div");
+        divWidth = 100/sideLength;
+        newDiv.style.width=`${divWidth}%`;
+        newDiv.style.height=`${divWidth}%`;
+        container.appendChild(newDiv)
+    divs = document.querySelectorAll(".container>div")
+    divs.forEach(function(div){
+        div.addEventListener("mouseover",function(e){
+            div.style.backgroundColor="black";
+        })
+    })    
+}
 }
 
-divs = document.querySelectorAll(".container>div")
+gridCreation()
 
-divs.forEach(function(div){
-    div.addEventListener("mouseover",function(e){
-        div.style.backgroundColor="black";
-    })
+button = document.querySelector("button");
+button.addEventListener("click",function(e){
+    let gridSize = parseInt(prompt("Number of Squares per side?"))
+    gridCreation(gridSize)
 })
